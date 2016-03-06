42e95cb25311d8afa6c5ca6a55b185e29c8fc8d1
Brian Douglas <ilikerobot@gmail.com>
2016-03-06 04:52:22 -0800

    adds stacks

--- stacks_and_queues/assignment.go
+++ stacks_and_queues/assignment.go
@@ -2,6 +2,37 @@ package main
 
 import ()
 
-type MyStack struct {
-	items []int
+type Stack struct {
+	top  *Element
+	size int
+}
+
+type Element struct {
+	value interface{}
+	stack *Stack
+	next  *Element
+}
+
+func (s *Stack) isEmpty() bool {
+	if s.size > 0 {
+		return false
+	}
+
+	return true
+}
+
+func (s *Stack) Push(value interface{}) {
+	s.top = &Element{value: value, stack: s, next: s.top}
+	s.size++
+}
+
+func (s *Stack) Pop() interface{} {
+	if s.size > 0 {
+		value := s.top.value
+		s.top = s.top.next
+		s.size--
+		return value
+	}
+
+	return nil
 }
--- stacks_and_queues/assignment_test.go
+++ stacks_and_queues/assignment_test.go
@@ -6,9 +6,25 @@ import (
 )
 
 func TestInitializationOfMystack(t *testing.T) {
-	m := new(MyStack)
+	m := new(Stack)
 	assert.NotNil(t, m)
 }
 
+func TestStackIsInitializedEmpty(t *testing.T) {
+	s := new(Stack)
+	assert.Equal(t, true, s.isEmpty())
+}
+
 func TesdtItemCanBePushedOntoStack(t *testing.T) {
+	s := new(Stack)
+	s.Push("12")
+	assert.Equal(t, false, s.isEmpty())
+}
+
+func TesdtItemCanBePopOffStack(t *testing.T) {
+	s := new(Stack)
+	s.Push("7")
+	assert.Equal(t, false, s.isEmpty())
+	s.Pop()
+	assert.Equal(t, true, s.isEmpty())
 }

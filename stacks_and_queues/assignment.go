a773f5f882cd26ca965b323eb9d91584a6072158
Brian Douglas <ilikerobot@gmail.com>
2016-03-07 08:15:46 -0800

    stack queue

--- stacks_and_queues/assignment.go
+++ stacks_and_queues/assignment.go
@@ -8,12 +8,13 @@ type Stack struct {
 }
 
 type Element struct {
-	value interface{}
-	stack *Stack
-	next  *Element
+	value    interface{}
+	stack    *Stack
+	next     *Element
+	previous *Element
 }
 
-func (s *Stack) isEmpty() bool {
+func (s *Stack) IsEmpty() bool {
 	if s.size > 0 {
 		return false
 	}
@@ -36,3 +37,99 @@ func (s *Stack) Pop() interface{} {
 
 	return nil
 }
+
+type Queue struct {
+	top  *Element
+	tail *Element
+	size int
+}
+
+func (q *Queue) IsEmpty() bool {
+	if q.size > 0 {
+		return false
+	}
+
+	return true
+}
+
+func (q *Queue) Enqueue(value interface{}) {
+	q.size++
+	if q.size == 1 {
+		q.top = &Element{value: value, previous: nil}
+		q.tail = q.top
+		return
+	}
+	q.tail.previous = &Element{value: value, previous: nil}
+	q.tail = q.tail.previous
+}
+
+func (q *Queue) Dequeue() interface{} {
+	if !q.IsEmpty() {
+		value := q.top.value
+		q.top = q.top.previous
+		q.size--
+		return value
+	}
+
+	return nil
+}
+
+type StackQueue struct {
+	inbox  *Stack
+	outbox *Stack
+}
+
+func (q *StackQueue) IsEmpty() bool {
+	if q.inbox.IsEmpty() && q.outbox.IsEmpty() {
+		return true
+	}
+
+	return false
+}
+
+func (q *StackQueue) Enqueue(value interface{}) {
+	q.inbox.Push(value)
+}
+
+func (q *StackQueue) resetInbox() {
+	for q.inbox.IsEmpty() {
+		q.outbox.Push(q.inbox.Pop())
+	}
+}
+
+func (q *StackQueue) Dequeue() interface{} {
+	if !q.IsEmpty() {
+		q.outbox.Push(q.inbox.Pop())
+	}
+
+	return nil
+}
+
+// type QueueStack struct {
+// 	inbox  *Queue
+// 	outbox *Queue
+// }
+
+// func (s *StackQueue) IsEmpty() bool {
+// 	if s.inbox.IsEmpty() && s.outbox.IsEmpty() {
+// 		return true
+// 	}
+
+// 	return false
+// }
+
+// func (s *Queue) Push(value interface{}) {
+// 	s.top = &Element{value: value, stack: s, next: s.top}
+// 	s.size++
+// }
+
+// func (s *StackQueue Pop() interface{} {
+// 	if s.size > 0 {
+// 		value := s.top.value
+// 		s.top = s.top.next
+// 		s.size--
+// 		return value
+// 	}
+
+// 	return nil
+// }
--- stacks_and_queues/assignment_test.go
+++ stacks_and_queues/assignment_test.go
@@ -5,26 +5,103 @@ import (
 	"testing"
 )
 
-func TestInitializationOfMystack(t *testing.T) {
-	m := new(Stack)
-	assert.NotNil(t, m)
-}
-
 func TestStackIsInitializedEmpty(t *testing.T) {
 	s := new(Stack)
-	assert.Equal(t, true, s.isEmpty())
+	assert.Equal(t, true, s.IsEmpty())
 }
 
-func TesdtItemCanBePushedOntoStack(t *testing.T) {
+func TestItemCanBePushedOntoStack(t *testing.T) {
 	s := new(Stack)
 	s.Push("12")
-	assert.Equal(t, false, s.isEmpty())
+	assert.Equal(t, false, s.IsEmpty())
 }
 
-func TesdtItemCanBePopOffStack(t *testing.T) {
+func TestItemCanBePopOffStack(t *testing.T) {
 	s := new(Stack)
 	s.Push("7")
-	assert.Equal(t, false, s.isEmpty())
+	assert.Equal(t, false, s.IsEmpty())
 	s.Pop()
-	assert.Equal(t, true, s.isEmpty())
+	assert.Equal(t, true, s.IsEmpty())
+}
+
+func TestStackIsLIFO(t *testing.T) {
+	s := new(Stack)
+	s.Push("First")
+	s.Push("Last")
+	s.Pop()
+	top := s.top.value
+	assert.Equal(t, "First", top)
+}
+
+func TestQueueIsInitializedEmpty(t *testing.T) {
+	q := new(Queue)
+	assert.Equal(t, true, q.IsEmpty())
+}
+
+func TestItemCanBeQueued(t *testing.T) {
+	q := new(Queue)
+	q.Enqueue("12")
+	assert.Equal(t, false, q.IsEmpty())
+}
+
+func TestItemCanBeDequeued(t *testing.T) {
+	q := new(Queue)
+	q.Enqueue("12")
+	assert.Equal(t, false, q.IsEmpty())
+	q.Dequeue()
+	assert.Equal(t, true, q.IsEmpty())
+}
+
+func TestFIFO(t *testing.T) {
+	q := new(Queue)
+	q.Enqueue("First")
+	q.Enqueue("Last")
+	q.Dequeue()
+	top := q.top.value
+	assert.Equal(t, "Last", top)
+}
+
+func TestStackQueueCanHold2Stacks(t *testing.T) {
+	one := new(Stack)
+	two := new(Stack)
+	q := StackQueue{one, two}
+	assert.NotNil(t, q)
+}
+
+func TestStackQueueIsEmpty(t *testing.T) {
+	one := new(Stack)
+	two := new(Stack)
+	q := StackQueue{one, two}
+	assert.Equal(t, true, q.IsEmpty())
+}
+
+func TestStackQueueCanEnqueue(t *testing.T) {
+	one := new(Stack)
+	two := new(Stack)
+	q := StackQueue{one, two}
+	q.Enqueue("stuff")
+	inboxValue := q.inbox.top.value
+	assert.Equal(t, "stuff", inboxValue)
+}
+
+func TestStackQueueCanDequeueAndMaintainTheStackLIFO(t *testing.T) {
+	one := new(Stack)
+	two := new(Stack)
+	q := StackQueue{one, two}
+	q.Enqueue("stuff")
+	q.Enqueue("more stuff")
+	q.Dequeue()
+	inboxValue := q.inbox.top.value
+	assert.Equal(t, "stuff", inboxValue)
+}
+
+func TestStackQueuePopulatesOutboxWhenOutBoxIsEmpty(t *testing.T) {
+	in := new(Stack)
+	out := new(Stack)
+	q := StackQueue{in, out}
+	q.Enqueue("a thing")
+	q.Enqueue("another thing")
+	q.Dequeue()
+	outboxValue := q.outbox.top.value
+	assert.Equal(t, "another thing", outboxValue)
 }

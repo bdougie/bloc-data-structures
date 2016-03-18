c067587efd92e576d5caf434ddf7035dda52e6aa
Brian Douglas <ilikerobot@gmail.com>
2016-03-17 17:28:02 -0700

    completed linked list

--- linked_lists/assignment.go
+++ linked_lists/assignment.go
@@ -89,3 +89,56 @@ func (l *LinkedList) RemoveFront() *Node {
 	l.head = l.head.next
 	return n
 }
+
+// LinkedListStack
+
+type LinkedListStack struct {
+	top  *Node
+	size int
+}
+
+func (s *LinkedListStack) Push(value interface{}) {
+	s.top = &Node{value: value, next: s.top}
+	s.size++
+}
+
+func (s *LinkedListStack) Pop() interface{} {
+	if s.size > 0 {
+		value := s.top.value
+		s.top = s.top.next
+		s.size--
+		return value
+	}
+
+	return nil
+}
+
+// LinkedListQueue
+
+type LinkedListQueue struct {
+	top  *Node
+	tail *Node
+	size int
+}
+
+func (q *LinkedListQueue) Enqueue(value interface{}) {
+	q.size++
+	if q.size == 1 {
+		q.top = &Node{value: value, next: nil}
+		q.tail = q.top
+		return
+	}
+	q.tail.next = &Node{value: value, next: nil}
+	q.tail = q.tail.next
+}
+
+func (q *LinkedListQueue) Dequeue() interface{} {
+	if q.size > 0 {
+		value := q.top.value
+		q.top = q.top.next
+		q.size--
+		return value
+	}
+
+	return nil
+}
--- linked_lists/assignment_test.go
+++ linked_lists/assignment_test.go
@@ -84,3 +84,116 @@ func TestNodeCanBeRemovedFromHead(t *testing.T) {
 
 	assert.Equal(t, "oranges", n.value)
 }
+
+// Benchmarking
+// go test -bench=.
+
+// Compare the time it takes to create a 10,000 item Array to appending 10,000
+// items to a Linked List.
+func BenchLinkedListCreation(b *testing.B) *LinkedList {
+	l := new(LinkedList)
+
+	for n := 0; n < b.N; n++ {
+		l.AddToTail(n)
+	}
+
+	return l
+	// runtime: 2.648s
+}
+
+func BenchArrayCreation(b *testing.B) {
+	slice := make([]int, 0, 10000)
+
+	for i := 0; i < 10000; i++ {
+		slice = append(slice, i)
+	}
+
+	// runtime: 0.009s
+}
+
+// Compare the time it takes to access the 5000th element of the Array and the
+// 5000th Node in the Linked List.
+func createLinkedList() *LinkedList {
+	l := new(LinkedList)
+
+	for n := 0; n < 10000; n++ {
+		l.AddToTail(n)
+	}
+
+	return l
+	// runtime: 0.008s
+}
+
+var accessLinked = createLinkedList()
+
+func BenchLinkedListAccess(b *testing.B) *Node {
+	head := accessLinked.head
+	for i := 0; i == 5000; i++ {
+		head = head.next
+	}
+
+	return head
+	// runtime: 0.009s
+}
+
+var aSlice [10000]int
+
+func BenchArrayAccess() int {
+	return aSlice[5000+1]
+	// runtime: 0.008s
+}
+
+//Compare the time it takes to remove the 5000th element from the Array to
+//removing the 5000th Node in the Linked List.
+var removeLinked = createLinkedList()
+
+func BenchmarkLinkedListRemoval(b *testing.B) {
+	head := removeLinked.head
+
+	for i := 0; i == 5000; i++ {
+		head = head.next
+	}
+
+	removeLinked.delete(head)
+	// runtime: 0.026s
+}
+
+func removeFromLine() []int {
+	return append(aSlice[:5000], aSlice[5000+1:]...)
+}
+
+func BenchmarkArrayRemoval(b *testing.B) {
+	removeFromLine()
+	// runtime: 0.009s
+}
+
+// LinkedListStack
+
+func TestItemCanBePushedOntoStack(t *testing.T) {
+	s := new(LinkedListStack)
+	s.Push("12")
+	assert.Equal(t, 1, s.size)
+}
+
+func TestItemCanBePopOffStack(t *testing.T) {
+	s := new(LinkedListStack)
+	s.Push("7")
+	s.Pop()
+	assert.Equal(t, 0, s.size)
+}
+
+// LinkedListQueue
+
+func TestItemCanBeQueued(t *testing.T) {
+	q := new(LinkedListQueue)
+	q.Enqueue("12")
+	assert.Equal(t, 1, q.size)
+}
+
+func TestItemCanBeDequeued(t *testing.T) {
+	q := new(LinkedListQueue)
+	q.Enqueue("12")
+	assert.Equal(t, 1, q.size)
+	q.Dequeue()
+	assert.Equal(t, 0, q.size)
+}
